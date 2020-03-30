1dc46ea450e9025aeeacae8927129c056f83d378
Devil7DK <dineshthangavel47@gmail.com>
2020-03-30 11:44:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Devil7.Utils.GDriveCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
