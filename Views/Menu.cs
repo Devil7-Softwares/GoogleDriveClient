95a8ea4e7412503b4f41a68c447a60fc16a099eb
Devil7DK <dineshthangavel47@gmail.com>
2020-04-01 13:36:57 +0530

    Add Initial Menu Bar

---
+++ Views/Menu.cs
@@ -0,0 +1,18 @@
+﻿using Terminal.Gui;
+
+namespace Devil7.Utils.GDriveCLI.Views
+{
+    class Menu
+    {
+        public static void Create()
+        {
+            MenuBar menu = new MenuBar(new MenuBarItem[] {
+                new MenuBarItem ("_File", new MenuItem [] {
+                    new MenuItem ("_Quit", "", Dialogs.Quit)
+                })
+            });
+
+            Application.Top.Add(menu);
+        }
+    }
+}
