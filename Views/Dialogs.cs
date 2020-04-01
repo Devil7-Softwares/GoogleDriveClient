1d7c70904bbb2246662f253309a920b929e2bdb3
Devil7DK <dineshthangavel47@gmail.com>
2020-04-01 13:36:48 +0530

    Add Dialogs Module with Quit Dialog

---
+++ Views/Dialogs.cs
@@ -0,0 +1,19 @@
+﻿using Terminal.Gui;
+
+namespace Devil7.Utils.GDriveCLI.Views
+{
+    class Dialogs
+    {
+        public static void Quit()
+        {
+            int dialogResult = MessageBox.Query(50, 7, "Quit", "Are you sure you want to quit?", "Yes", "No");
+            if (dialogResult == 0)
+            {
+                Application.Top.Running = false;
+            } else
+            {
+                Application.Top.SetFocus(MyDrive.Window);
+            }
+        }
+    }
+}
