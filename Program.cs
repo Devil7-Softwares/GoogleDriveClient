67b02ff526332d135706f28aa53527b68b363f1e
Devil7DK <dineshthangavel47@gmail.com>
2020-04-01 13:37:41 +0530

    Implement Authendication & Loading UI Flow

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using Terminal.Gui;
 
 namespace Devil7.Utils.GDriveCLI
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            if (Utils.Drive.IsAuthorized())
+            {
+                Console.WriteLine("Application already authorized!");
+            }
+            else
+            {
+                if (Utils.Drive.Authorize())
+                {
+                    Console.WriteLine("Authorization successful!");
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (Utils.Drive.Init())
+            {
+                Console.WriteLine("API initialization successful! Loading UI...");
+            }
+            else
+            {
+                Console.WriteLine("API initialization failed!");
+                return;
+            }
+
+            Application.Init();
+
+            Views.MyDrive.Create();
+            Views.Menu.Create();
+
+            Application.Run();
         }
     }
 }
