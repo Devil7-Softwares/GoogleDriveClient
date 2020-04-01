4768021dd51053fa43c44db33b7de40556db0a7e
Devil7DK <dineshthangavel47@gmail.com>
2020-04-01 13:36:10 +0530

    Add Constants Module

---
+++ Utils/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Devil7.Utils.GDriveCLI.Utils
+{
+    public class Constants
+    {
+        public const string REGEX_AUTH_ERROR = ".*\\(Error:\\\"(?<Error>.*)\\\", Description:\\\"(?<Description>.*)\\\", Uri:\\\"(?<Uri>.*)\\\"\\)";
+
+        public const string MIME_GDRIVE_DIRECTORY = "application/vnd.google-apps.folder";
+        public const string URI_GDRIVE_REDIRECT = "urn:ietf:wg:oauth:2.0:oob";
+    }
+}
