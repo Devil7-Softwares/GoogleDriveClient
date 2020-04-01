ed64490a722065127dc7c72a2124a3c23c8ffcf2
Devil7DK <dineshthangavel47@gmail.com>
2020-04-01 16:01:23 +0530

    Implement Comparer for FileListItem

---
+++ Utils/FileListItemComparer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Devil7.Utils.GDriveCLI.Utils
+{
+    class FileListItemComparer : IComparer<Models.FileListItem>
+    {
+        public int Compare([AllowNull] Models.FileListItem x, [AllowNull] Models.FileListItem y)
+        {
+            if (Settings.SortBy == SortBy.Name)
+            {
+                return Settings.SortOrder == SortOrder.Ascending ? x.Name.CompareTo(y.Name) : y.Name.CompareTo(x.Name);
+            }
+            else
+            {
+                return Settings.SortOrder == SortOrder.Ascending ? x.LastModified.CompareTo(y.LastModified) : y.LastModified.CompareTo(x.LastModified);
+            }
+        }
+    }
+}
