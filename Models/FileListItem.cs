0b3fe14b3489b9842177829f3f8bf028a2362e13
Devil7DK <dineshthangavel47@gmail.com>
2020-04-01 13:25:40 +0530

    Create Model for Listing File

---
+++ Models/FileListItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Devil7.Utils.GDriveCLI.Models
+{
+    public class FileListItem
+    {
+        #region Properties
+        public string Id { get; }
+        public string Name { get; }
+        public string Owner { get; }
+        public DateTime LastModified { get; }
+        public long FileSize { get; }
+
+        public bool IsDirectory { get; }
+        public bool IsSelected { get; internal set; }
+        #endregion
+
+        #region Constructor
+        public FileListItem(string Id, string Name, string Owner, DateTime LastModified, long FileSize, string MimeType)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Owner = Owner;
+            this.LastModified = LastModified;
+            this.FileSize = FileSize;
+
+            this.IsDirectory = (MimeType == Utils.Constants.MIME_GDRIVE_DIRECTORY);
+        } 
+        #endregion
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
