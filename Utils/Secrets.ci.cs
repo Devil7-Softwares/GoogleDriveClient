a437d20ca53f39c92f03dad8c76dfd6089ecf72b
Devil7DK <dineshthangavel47@gmail.com>
2020-04-15 18:49:40 +0530

    Add Dummy Secrets File to Use with CI

---
+++ Utils/Secrets.ci.cs
@@ -0,0 +1,9 @@
+﻿namespace Devil7.Utils.GoogleDriveClient.Utils
+{
+    internal class Secrets
+    {
+        public const string GDRIVE_CLIENT_ID = "${GDRIVE_CLIENT_ID}";
+        public const string GDRIVE_CLIENT_SECRET = "${GDRIVE_CLIENT_SECRET}";
+        public const string GDRIVE_PROJECT_ID = "${GDRIVE_PROJECT_ID}";
+    }
+}
